8a89196a6cdc55315f6861ce88bc66b68663e916
lapaduga <deniskotov29042015@gmail.com>
2020-09-12 04:21:26 +0500

    j

--- js/script.js
+++ js/script.js
@@ -5,11 +5,11 @@ $(document).ready(function () {
 		if (document.documentElement.clientWidth > 992) {
 			if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
 				document.getElementById("header__body").style.height = "80px";
+				document.getElementById("header").style.background = "#33aaffe7";
 			} else {
 				document.getElementById("header__body").style.height = "161px";
+				document.getElementById("header").style.background = "transparent";
 			}
-		} else {
-			document.getElementById("header__body").style.height = "80px";
 		}
 	}
 	//burger behaviour on click
