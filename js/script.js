1fdc1575bdfb500636ecfc362e87f5c4d5276bf2
lapaduga <deniskotov29042015@gmail.com>
2020-09-12 01:42:37 +0500

    f

--- js/script.js
+++ js/script.js
@@ -1,6 +1,19 @@
 $(document).ready(function () {
+	//burger behaviour
 	$('.header__burger').click(function (event) {
 		$('.header__burger, .header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
 	});
+	window.onscroll = function () { scrollFunction() };
+	function scrollFunction() {
+		if (document.documentElement.clientWidth > 992) {
+			if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+				document.getElementById("header__body").style.height = "80px";
+			} else {
+				document.getElementById("header__body").style.height = "161px";
+			}
+		} else {
+			document.getElementById("header__body").style.height = "80px";
+		}
+	}
 });
