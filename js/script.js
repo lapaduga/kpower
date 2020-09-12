f1568d9a81502e528c8d50930433b88879b094f4
lapaduga <deniskotov29042015@gmail.com>
2020-09-12 16:22:06 +0500

    burger menu fully done

--- js/script.js
+++ js/script.js
@@ -5,16 +5,32 @@ $(document).ready(function () {
 		if (document.documentElement.clientWidth > 992) {
 			if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
 				document.getElementById("header__body").style.height = "80px";
-				document.getElementById("header").style.background = "#33aaffe7";
 			} else {
 				document.getElementById("header__body").style.height = "161px";
-				document.getElementById("header").style.background = "transparent";
 			}
+		}
+		if(document.documentElement.clientWidth > 767 && document.documentElement.clientWidth < 992){
+			document.getElementById("header__body").style.height = "80px";
+		}
+		if (document.documentElement.clientWidth < 768){
+			document.getElementById("header__body").style.height = "50px";
+		}
+		if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+			$("#logo").attr("src","img/icons/header/blacklogo.svg");
+			document.getElementById("header").style.background = "#ffffff78";
+		} else {
+			$("#logo").attr("src","img/icons/header/whitelogo.svg");
+			document.getElementById("header").style.background = "transparent";
 		}
 	}
 	//burger behaviour on click
 	$('.header__burger').click(function (event) {
 		$('.header__burger, .header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
+		if($('.header__menu').hasClass('active')){
+			$("#logo").attr("src","img/icons/header/blacklogo.svg");
+		} else{
+			$("#logo").attr("src","img/icons/header/whitelogo.svg");
+		}
 	});
 });
