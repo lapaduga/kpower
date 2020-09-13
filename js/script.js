4db136fe4f8a6a0085ae85c2f717c2fe22c02f0a
lapaduga <deniskotov29042015@gmail.com>
2020-09-14 00:28:16 +0500

    g

--- js/script.js
+++ js/script.js
@@ -2,6 +2,18 @@ $(document).ready(function () {
 	//burger behaviour on scroll
 	window.onscroll = function () { scrollFunction() };
 	function scrollFunction() {
+		if (document.documentElement.clientWidth > 1830) {
+			if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+				$("#logo").attr("src", "img/icons/header/blacklogo.svg");
+				document.getElementById("header").style.background = " #ffffff";
+			} else {
+				$("#logo").attr("src", "img/icons/header/whitelogo.svg");
+				document.getElementById("header").style.background = "transparent";
+			}
+		} else{
+			document.getElementById("header").style.background = " #ffffff";
+			$("#logo").attr("src", "img/icons/header/blacklogo.svg");
+		}
 		if (document.documentElement.clientWidth > 992) {
 			if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
 				document.getElementById("header__body").style.height = "80px";
@@ -9,34 +21,47 @@ $(document).ready(function () {
 				document.getElementById("header__body").style.height = "161px";
 			}
 		}
-		if(document.documentElement.clientWidth > 767 && document.documentElement.clientWidth < 992){
+		if (document.documentElement.clientWidth > 767 && document.documentElement.clientWidth < 992) {
 			document.getElementById("header__body").style.height = "80px";
 		}
-		if (document.documentElement.clientWidth < 768){
+		if (document.documentElement.clientWidth < 768) {
 			document.getElementById("header__body").style.height = "50px";
-		}
-		if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
-			$("#logo").attr("src","img/icons/header/blacklogo.svg");
-			document.getElementById("header").style.background = " #ffffff";
-		} else {
-			$("#logo").attr("src","img/icons/header/whitelogo.svg");
-			document.getElementById("header").style.background = "transparent";
 		}
 	}
 	//burger behaviour on click
 	$('.header__burger').click(function (event) {
 		$('.header__burger, .header__menu').toggleClass('active');
 		$('body').toggleClass('lock');
-		if($('.header__menu').hasClass('active')){
-			$("#logo").attr("src","img/icons/header/blacklogo.svg");
-		} else{
-			$("#logo").attr("src","img/icons/header/whitelogo.svg");
-		}
 	});
 	//click on mouse teaser
 	$("#mainscreen__scrolldown").click(function (event) {
-		$('html, body').animate({scrollTop: '+=500px'}, 800);
+		$('html, body').animate({ scrollTop: '+=500px' }, 800);
 	});
 	//slick slider
-	$('.main__slider').slick();
+	$('.main__slider').slick({
+		arrows: true,
+		dots: true,
+		adaptiveHeight: true,
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		easing: 'ease',
+		infinite: false,
+		touchThreshold: 20,
+		centerMode: false,
+		variableWidth: false,
+		responsive: [
+			{
+				breakpoint: 768,
+				settings: {
+
+				}
+			}
+		],
+		appendArrows: $('.bottom-arrows'),
+		appendDots: $('.bottom-left')
+	});
+	$('.main__slider').on('afterChange', function (event, slick, currentSlide) {
+		$('#change-num').text('0' + (currentSlide + 1));
+	});
+	//video adaptation
 });
