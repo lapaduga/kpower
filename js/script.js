11e0668f20deb91a34364e04a77eea3423e6aaac
lapaduga <deniskotov29042015@gmail.com>
2020-09-15 18:36:13 +0500

    g

--- js/script.js
+++ js/script.js
@@ -86,4 +86,38 @@ $(document).ready(function () {
 			scrollTop: $(".main").offset().top  // класс объекта к которому приезжаем
 		}, 1000); // Скорость прокрутки
 	});
+	//scroll animation
+	const animItems = document.querySelectorAll('._anim-items');
+
+	if(animItems.length > 0){
+		window.addEventListener('scroll', animOnScroll);
+		function animOnScroll(){
+			for(let i = 0; i < animItems.length; i++){
+				const animItem = animItems[i];
+				const animItemHeight = animItem.offsetHeight;
+				const animItemOffset = offset(animItem).top;
+				const animStart = 4;
+
+				let animItemPoint = window.innerHeight - animItemHeight / animStart;
+				if(animItemHeight > window.innerHeight){
+					animItemPoint = window.innerHeight - window.innerHeight / animStart;
+				}
+
+				if((pageYOffset > animItemOffset - animItemPoint) && pageYOffset < (animItemOffset + animItemHeight)){
+					animItem.classList.add('_active');
+				} else{
+					animItem.classList.remove('_active');
+				}
+			}
+		}
+		function offset(el){
+			const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+			return {top: rect.top + scrollTop, left: rect.left + scrollLeft}
+		}
+	}
+	setTimeout(()=>{
+		animOnScroll();
+	}, 300);
 });
